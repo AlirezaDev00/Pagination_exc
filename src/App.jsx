4edcf0badcbcaea35b3908c0edc2d3df80bed0d6
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-05-05 10:36:46 +0330

    init project to git

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+import Users from "./Components/Users/Users"
+
+export default function App() {
+  return (
+    <>
+      <Users></Users>
+      {/* <Pagination></Pagination> */}
+    </>
+  );
+}
