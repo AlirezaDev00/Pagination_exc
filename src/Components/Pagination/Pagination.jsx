c427c12001caa77b28767957beed4e9287e50295
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-05-07 16:04:49 +0330

    coding pagination comp styles

--- src/Components/Pagination/Pagination.jsx
+++ src/Components/Pagination/Pagination.jsx
@@ -4,9 +4,9 @@ export default function Pagination({ set_current_page, pages }) {
   return (
     <section className="pagination_section">
       {pages.map((page) => (
-        <span key={page} onClick={() => set_current_page(page + 1)}>
+        <button type="button" key={page} onClick={() => set_current_page(page + 1)}>
           {page + 1}
-        </span>
+        </button>
       ))}
     </section>
   );
