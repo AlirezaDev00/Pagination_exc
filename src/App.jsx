c16b8a2b9a590910f6003386b7c88516acdc6de3
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-05-07 14:49:47 +0330

    coding table logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
-import Users from "./Components/Users/Users"
+import Users from "./Components/Users/Users";
 
 export default function App() {
+  const [todos, set_todos] = useState(null);
+  const [is_pending, set_is_pending] = useState(true);
+  const [error, set_error] = useState(null);
+
+  const [current_page , set_current_page] = useState(2)
+
+  const item_count = 10
+
+  const start_index = (current_page - 1) * item_count;
+
+  const end_index = current_page * item_count
+
+
+  useEffect(() => {
+    fetch("/src/Data/data.json")
+      .then((res) => res.json())
+      .then((data) => {
+        set_todos(data);
+        set_is_pending(false);
+      })
+      .catch((err) => {
+        set_error(err.message);
+
+        set_is_pending(false);
+      });
+  }, []);
+
   return (
     <>
-      <Users></Users>
+      {is_pending && <h2>Loading ...</h2>}
+      {todos && <Users todos={todos} start={start_index} end={end_index}></Users>}
+      {error === `Unexpected token '<', "<!doctype "... is not valid JSON` && (
+        <p>Url Is Not Valid!!</p>
+      )}
       {/* <Pagination></Pagination> */}
     </>
   );
--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -1,10 +1,12 @@
 import "./Users.css";
 
-export default function Users() {
+export default function Users({ todos , start , end }) {
+  const filtered_todos = todos.slice(start, end);
+
   return (
     <table className="users_table">
       <thead>
-        <tr className="js">
+        <tr>
           <th>ID</th>
           <th>UserId</th>
           <th>title</th>
@@ -12,36 +14,18 @@ export default function Users() {
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>5</td>
-          <td>10</td>
-          <td>Hello Im Alireza</td>
-          <td className="status"><span className="status_text completed">Completed</span></td>
-        </tr>
-        <tr>
-          <td>5</td>
-          <td>10</td>
-          <td>Hello Im Alireza</td>
-          <td className="status"><span className="status_text completed">Completed</span></td>
-        </tr>
-        <tr>
-          <td>5</td>
-          <td>10</td>
-          <td>Hello Im Alireza</td>
-          <td className="status"><span className="status_text completed">Completed</span></td>
-        </tr>
-        <tr>
-          <td>5</td>
-          <td>10</td>
-          <td>Hello Im Alireza</td>
-          <td className="status"> <span className="status_text un_completed">UnCompleted</span></td>
-        </tr>
-        <tr>
-          <td>5</td>
-          <td>10</td>
-          <td>Hello Im Alireza</td>
-          <td className="status"><span className="status_text un_completed">UnCompleted</span></td>
-        </tr>
+        {filtered_todos.map((todo) => (
+          <tr key={todo.id} id={todo.id === filtered_todos.length  ? "last_todo" : ""}>
+            <td>{todo.id}</td>
+            <td>{todo.user_id}</td>
+            <td>{todo.title.slice(0, 25) + "..."}</td>
+            <td className="status">
+              <span className={`status_text ${todo.is_completed}`}>
+                {todo.is_completed}
+              </span>
+            </td>
+          </tr>
+        ))}
       </tbody>
     </table>
   );
